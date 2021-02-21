40e1200f4fb70d924a7dec1062550cbf61bb2204
student <Muthoniyahya@gmail.com>
2021-02-21 10:06:07 +0300

    first commit

---
+++ src/hello.kt
@@ -0,0 +1,23 @@
+fun main() {
+var Name =("Iman Muthoni Yahya")
+println(Name)
+ var Age =(18)
+ println(Age)
+ var Phonenumber=("0115626974")
+ println(Phonenumber)
+ var Weight:Float=70.5 .toFloat()
+ println(Weight)
+ var Citizen:String="Kenyan"
+    println(Citizen)
+
+
+
+
+
+
+
+
+}
+
+
+
